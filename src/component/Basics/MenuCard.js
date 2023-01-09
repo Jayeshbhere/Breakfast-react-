43fdc162cc996fdff8a44a206248c29d8d56ceed
Jayeshbhere <79853968+Jayeshbhere@users.noreply.github.com>
2023-01-09 18:18:06 +0530

    initial commit

---
+++ src/component/Basics/MenuCard.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const MenuCard = ({menuData}) => {
+  return (
+    <>
+        <section className="main-card--container">
+            {menuData.map((curElem) =>{
+                return (
+                <>
+                    <div className="card-container key={curElem.id}">
+                        <div className="card">
+                            <div className="card-body">
+                                <span className="card-number card-circle subtle">{curElem.id}</span>
+                                <span className="card-author subtle">Breakfast</span>
+                                <h2 className="card-title">{curElem.name}</h2>
+                                <span className="card-description subtle">{curElem.description}</span>
+                                <div className="card-read">Read</div>
+                                <img src="{curElem.id}" alt="images" className="card-media" />
+                            </div>
+                        </div>
+                    </div>
+                </>)
+            })}
+        </section>
+        
+        
+    </>
+  )
+}
+
+export default MenuCard
---
+++ src/component/Basics/Restaurant.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import "./style.css";
+import Menu from './menuApi';
+import MenuCard from './MenuCard';
+
+const Restaurant = () => {
+
+  const [menuData, setMenuData] = useState(Menu);
+  return (
+    <>
+        <nav className="navbar">
+            <div className="btn-group">
+                <button className="btn-group__item">Breakfast</button>
+            </div>
+        </nav>
+        <MenuCard menuData={menuData} />
+    </>
+  )
+}
+
+export default Restaurant;
